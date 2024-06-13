690aef00c2027e26dac47f00f9131acc912e8883
ZyroGatsby <63269088+ZyroGatsby@users.noreply.github.com>
2024-06-13 18:01:35 +0100

    chore: update main page :recycle:

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,26 +13,31 @@ export const metadata: Metadata = {
  * You can override the next-env if the type is important to you
  * @see https://stackoverflow.com/questions/68103844/how-to-override-next-js-svg-module-declaration
  */
-import { PlusIcon } from 'lucide-react';
 
 import Logo from '~/svg/Logo.svg';
 
 const cards = [
   {
-    name: 'Lindsay Walton',
-    role: 'Front-end Developer',
+    name: 'Machine Learning',
+    description: 'Learn the basics of machine...',
     imageUrl:
       'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
   },
   {
-    name: 'Courtney Henry',
-    role: 'Designer',
+    name: 'Calculus and Statistics',
+    description: 'Learn and enhance your abil...',
     imageUrl:
       'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
   },
   {
-    name: 'Tom Cook',
-    role: 'Director, Product Development',
+    name: 'Project Management',
+    description: 'Become a seasoned project...',
+    imageUrl:
+      'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+  },
+  {
+    name: 'UI/UX Design',
+    description: 'Become a seasoned project...',
     imageUrl:
       'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
   },
@@ -41,7 +46,7 @@ const cards = [
 export default function HomePage() {
   return (
     <main>
-      <div className='mx-auto max-w-lg'>
+      <div className='mx-auto max-w-lg px-4 md:px-6 py-12'>
         <div>
           <div className='text-center'>
             <Logo className='w-10 mx-auto' />
@@ -88,17 +93,17 @@ export default function HomePage() {
                   <div className='flex-shrink-0'>
                     {/* eslint-disable-next-line @next/next/no-img-element */}
                     <img
-                      className='h-10 w-10 rounded-full'
+                      className='h-10 w-10 rounded-md'
                       src={card.imageUrl}
                       alt=''
                     />
                   </div>
                   <div className='min-w-0 flex-1'>
-                    <p className='truncate text-sm font-medium text-gray-900'>
+                    <p className='truncate text-sm font-normal text-gray-900'>
                       {card.name}
                     </p>
-                    <p className='truncate text-sm font-medium text-gray-500'>
-                      {card.role}
+                    <p className='truncate text-sm font-normal text-gray-500'>
+                      {card.description}
                     </p>
                   </div>
                 </div>
@@ -107,11 +112,7 @@ export default function HomePage() {
                     type='button'
                     className='inline-flex items-center rounded-full border border-transparent bg-gray-100 py-2 px-3 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'
                   >
-                    <PlusIcon
-                      className='-ml-1 mr-0.5 h-5 w-5 text-gray-400'
-                      aria-hidden='true'
-                    />
-                    <span className='text-sm font-medium text-gray-900'>
+                    <span className='text-sm text-gray-900'>
                       Start learning
                     </span>
                   </button>
