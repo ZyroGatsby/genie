35789bc6c007fd8d6118654323b79faa35d191e1
ZyroGatsby <63269088+ZyroGatsby@users.noreply.github.com>
2024-06-13 16:51:08 +0100

    chore(dep): set up shadcn :heavy_plus_sign:

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,24 +7,72 @@ export default {
       fontFamily: {
         primary: ['var(--font-archivo)'],
       },
+      borderRadius: {
+        lg: `var(--radius)`,
+        md: `calc(var(--radius) - 2px)`,
+        sm: "calc(var(--radius) - 4px)",
+      },
+      // colors: {
+      //   primary: {
+      //     // Customize it on globals.css :root
+      //     50: 'rgb(var(--tw-color-primary-50) / <alpha-value>)',
+      //     100: 'rgb(var(--tw-color-primary-100) / <alpha-value>)',
+      //     200: 'rgb(var(--tw-color-primary-200) / <alpha-value>)',
+      //     300: 'rgb(var(--tw-color-primary-300) / <alpha-value>)',
+      //     400: 'rgb(var(--tw-color-primary-400) / <alpha-value>)',
+      //     500: 'rgb(var(--tw-color-primary-500) / <alpha-value>)',
+      //     600: 'rgb(var(--tw-color-primary-600) / <alpha-value>)',
+      //     700: 'rgb(var(--tw-color-primary-700) / <alpha-value>)',
+      //     800: 'rgb(var(--tw-color-primary-800) / <alpha-value>)',
+      //     900: 'rgb(var(--tw-color-primary-900) / <alpha-value>)',
+      //     950: 'rgb(var(--tw-color-primary-950) / <alpha-value>)',
+      //   },
+      //   dark: '#222222',
+      // },
       colors: {
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
         primary: {
-          // Customize it on globals.css :root
-          50: 'rgb(var(--tw-color-primary-50) / <alpha-value>)',
-          100: 'rgb(var(--tw-color-primary-100) / <alpha-value>)',
-          200: 'rgb(var(--tw-color-primary-200) / <alpha-value>)',
-          300: 'rgb(var(--tw-color-primary-300) / <alpha-value>)',
-          400: 'rgb(var(--tw-color-primary-400) / <alpha-value>)',
-          500: 'rgb(var(--tw-color-primary-500) / <alpha-value>)',
-          600: 'rgb(var(--tw-color-primary-600) / <alpha-value>)',
-          700: 'rgb(var(--tw-color-primary-700) / <alpha-value>)',
-          800: 'rgb(var(--tw-color-primary-800) / <alpha-value>)',
-          900: 'rgb(var(--tw-color-primary-900) / <alpha-value>)',
-          950: 'rgb(var(--tw-color-primary-950) / <alpha-value>)',
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
         },
-        dark: '#222222',
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
       },
       keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
         flicker: {
           '0%, 19.999%, 22%, 62.999%, 64%, 64.999%, 70%, 100%': {
             opacity: '0.99',
@@ -46,10 +94,12 @@ export default {
         },
       },
       animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
         flicker: 'flicker 3s linear infinite',
         shimmer: 'shimmer 1.3s linear infinite',
       },
     },
   },
-  plugins: [require('@tailwindcss/forms')],
+  plugins: [require('@tailwindcss/forms'), require("tailwindcss-animate")],
 } satisfies Config;
