1c9006e793de57426284ec3cc9ca6896e212f7ae
ZyroGatsby <63269088+ZyroGatsby@users.noreply.github.com>
2024-06-16 10:31:23 +0100

    style: update input component to neobrutalism :lipstick:

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,8 +1,8 @@
+/* eslint-disable @typescript-eslint/no-empty-interface */
 import * as React from 'react';
 
 import { cn } from '@/lib/utils';
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {}
 
@@ -12,7 +12,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          'flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-0 disabled:cursor-not-allowed disabled:opacity-50',
+          'flex h-10 w-full rounded-base border-2 font-base selection:bg-main selection:text-black border-black bg-white px-3 py-2 text-sm ring-offset-white file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-slate-500 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-slate-950/50 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
           className
         )}
         ref={ref}
