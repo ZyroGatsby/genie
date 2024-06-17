e8db58f10c198baa76e51696ecb45617a8cc6fe9
ZyroGatsby <63269088+ZyroGatsby@users.noreply.github.com>
2024-06-17 19:27:25 +0100

    :recycle: Setup clerk auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Archivo as FontSans } from "next/font/google";
 import "./globals.css";
+import { Providers } from "./providers";
 
 const fontSans = FontSans({ subsets: ["latin"], variable: "--font-sans" });
 
@@ -16,7 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={fontSans.className}>{children}</body>
+      <body className={fontSans.className}>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
---
+++ convex/auth.config.ts
@@ -0,0 +1,9 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+    providers: [
+        {
+            domain: "https://relative-labrador-19.clerk.accounts.dev",
+            applicationID: "convex"
+        }
+    ]
+};
